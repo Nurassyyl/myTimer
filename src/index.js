25c74f5525be165f2d16e4731960a6e4d5c95d8a
unknown <abdildabekn@mail.ru>
2022-09-15 16:25:02 +0600

    Чтобы заработала лессон 24

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import f from "./modules/fetch";
+import sendForm from "./modules/sendForm";
 
 timer('15 September 2022');
 menu();
 modal();
-f()
+f();
+sendForm({formId: 'form1', someElem: [
+    {
+        type: 'block',
+        id: 'total'
+    }
+]});
